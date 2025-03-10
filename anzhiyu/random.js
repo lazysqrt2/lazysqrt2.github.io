d3b2d75900d18ed215c338bc591d754135f2fc49
Wjy <805813606@qq.com>
2025-03-10 19:46:04 +0800

    Site updated: 2025-03-10 19:45:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/03/hello-world/","2025/03/07/pytorch学习笔记1/","2025/03/07/计算机组成原理学习笔记(1)/"];function toRandomPost(){
+var posts=["2025/03/03/hello-world/","2025/03/07/PyTorch学习笔记(1)/","2025/03/07/计算机组成原理学习笔记(1)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
