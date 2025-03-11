684f05b5acee1d246360c48a11db842791b4533d
Wjy <805813606@qq.com>
2025-03-11 19:59:13 +0800

    Site updated: 2025-03-11 19:58:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/07/PyTorch学习笔记(1)/","2025/03/10/PyTorch学习笔记(2)/","2025/03/07/计算机组成原理学习笔记(1)/","2025/03/10/PyTorch学习笔记(3)/","2025/03/03/hello-world/"];function toRandomPost(){
+var posts=["2025/03/07/PyTorch学习笔记(1)/","2025/03/10/PyTorch学习笔记(3)/","2025/03/07/PyTorch学习笔记(4)/","2025/03/10/PyTorch学习笔记(2)/","2025/03/07/计算机组成原理学习笔记(1)/","2025/03/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
